9f829f833270d7ded3b0a0743af16f9c289e3123
cRexxar <499425216@qq.com>
2023-04-18 23:44:20 +0800

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,51 @@
+#[derive(Debug, Clone, Copy)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height
+    }
+
+    fn add1(&mut self) {
+        self.width += 1;
+        self.height += 1;
+    }
+
+    fn new_square(size: u32) -> Self {
+        return Self { width: size, height: size};
+    }
+}
+
+#[derive(Debug)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl  Point {
+    fn distance(&self, other:&Point) -> f64 {
+        let x_squared = f64::powi(other.x - self.x, 2);
+        let y_squared = f64::powi(other.y - self.y, 2);
+        return f64::sqrt(x_squared + y_squared)
+    }
+}
 
 fn main() {
-    let a: [i32; 3] = [1, 2, 3];
-    println!("{}", a[0]);
-    println!("{}", a[1]);
+    let mut rect1 = Rectangle::new_square(30);
+    rect1.add1();
+    // let area = get_area(&rect1);
+    let area = rect1.area();
+    println!("rect1 is {:?}, its area is {area}", rect1);
 
-    let mut s = String::from("hello world");
-    let hello = &s[..5];
-    let world = &s[5..];
-    let length = len_string(&s);
-    println!("{hello} {world}");
-    println!("{length}");
-    s.clear();
+    let p1 = Point{x: 0., y: 0.};
+    let p2 = Point{x: 3., y: 4.};
+    let distance = p1.distance(&p2);
+    println!("the distance during {:?} and {:?} is {distance}.", p1, p2);
 }
 
-// fn print_string(s: &mut String) -> &mut String{
-//     s.push_str("world");
-//     return s;
+// fn get_area(rect:&Rectangle) -> u32 {
+//     return rect.height*rect.width
 // }
-
-fn len_string(s: &String) -> &str {
-    return s
-}
