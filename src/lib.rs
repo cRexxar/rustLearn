59035319d8e7043d9aefab082128edbbd0fc5f94
cRexxar <499425216@qq.com>
2023-04-23 23:39:39 +0800

    doc

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+/// Adds one to the number given
+///
+/// # Examples
+///
+/// ```
+/// let num = 5;
+/// let sum = rust_learn::add_one(num);
+///
+/// assert_eq!(6, sum);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    return x + 1;
+}
+
+// fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-fn main() {
-    let add_closure = |x| x + 1;
-    let n = add_closure(5);
-    println!("{n}");
+use rust_learn;
 
-    let v1 = vec![1, 2, 3];
-    let res:Vec<i32> = v1.iter().map(|x| x + 1).collect();
-    println!("{:?}", res);
+fn main() {
+    let res = rust_learn::add_one(5);
+    println!("{res}")
 }
